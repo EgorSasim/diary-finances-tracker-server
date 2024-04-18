2e08774913be981a716b6763da40025f6ab2a723
egor sasim <egorsasim@mail.ru>
2024-04-18 09:01:56 +0300

    map task in appropriate way

--- src/test/test.constants.ts
+++ src/test/test.constants.ts
@@ -18,7 +18,7 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
       {
         id: 19,
         title: 'Volunteer for a cause you care about',
-        creationDate: new Date(Date.now()), // April 1, 2024
+        creationDate: new Date(Date.now()),
         description: 'Dedicate some time to volunteering for a good cause.',
         priority: 'High',
         startDate: new Date(Date.now()),
@@ -29,17 +29,18 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
       {
         id: 20,
         title: 'Get a haircut',
-        creationDate: new Date(Date.now()), // April 17, 2024 (today)
+        creationDate: new Date(Date.now()),
         description: 'Schedule an appointment for a haircut.',
         priority: 'Low',
         status: 'ToDo',
         startDate: new Date(Date.now()),
+        endDate: new Date(Date.now() + 12 * 60 * 60 * 1000),
         user,
       },
       {
         id: 21,
         title: 'Learn a new party trick',
-        creationDate: new Date(Date.now()), // April 17, 2024 (today)
+        creationDate: new Date(Date.now()),
         description: 'Impress your friends with a fun and unexpected skill.',
         priority: 'Low',
         startDate: new Date(Date.now()),
@@ -54,19 +55,19 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 1,
       title: 'Write a blog post',
-      creationDate: new Date(2024, 4, 16), // April 16, 2024
+      creationDate: new Date(2024, 4, 16),
       description: 'Research and write a blog post on a chosen topic.',
       priority: 'Medium',
-      startDate: new Date(2024, 4, 17), // April 17, 2024 (today)
-      endDate: new Date(2024, 4, 19), // April 19, 2024
-      reminder: new Date(2024, 4, 18), // April 18, 2024 (reminder for tomorrow)
+      startDate: new Date(2024, 4, 17),
+      endDate: new Date(2024, 4, 19),
+      reminder: new Date(2024, 4, 18),
       status: 'ToDo',
       user,
     },
     {
       id: 2,
       title: 'Grocery shopping',
-      creationDate: new Date(2024, 4, 15), // April 15, 2024
+      creationDate: new Date(2024, 4, 15),
       description: 'Buy milk, bread, vegetables, and fruits.',
       priority: 'High',
       status: 'InProgress',
@@ -76,10 +77,10 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 3,
       title: 'Clean the apartment',
-      creationDate: new Date(2024, 4, 12), // April 12, 2024
+      creationDate: new Date(2024, 4, 12),
       description: 'Vacuum, dust, mop floors, clean bathrooms and kitchen.',
       priority: 'Low',
-      endDate: new Date(2024, 4, 14), // April 14, 2024 (past due)
+      endDate: new Date(2024, 4, 14),
       status: 'Done',
 
       user,
@@ -87,10 +88,10 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 4,
       title: 'Call dentist for appointment',
-      creationDate: new Date(2024, 4, 10), // April 10, 2024
+      creationDate: new Date(2024, 4, 10),
       description: 'Schedule a checkup and cleaning.',
       priority: 'Medium',
-      reminder: new Date(2024, 4, 17), // April 17, 2024 (reminder for today)
+      reminder: new Date(2024, 4, 17),
       status: 'ToDo',
 
       user,
@@ -98,7 +99,7 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 5,
       title: 'Finish reading that book',
-      creationDate: new Date(2024, 3, 20), // March 20, 2024
+      creationDate: new Date(2024, 3, 20),
       description: 'Read the remaining chapters of the book I started.',
       priority: 'Low',
       status: 'InProgress',
@@ -108,11 +109,11 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 6,
       title: 'Exercise',
-      creationDate: new Date(2024, 4, 16), // April 16, 2024
+      creationDate: new Date(2024, 4, 16),
       description: 'Go for a run or do a workout routine.',
       priority: 'Medium',
-      startDate: new Date(2024, 4, 17), // April 17, 2024 (today)
-      reminder: new Date(2024, 4, 18), // April 18, 2024 (reminder for tomorrow)
+      startDate: new Date(2024, 4, 17),
+      reminder: new Date(2024, 4, 18),
       status: 'ToDo',
 
       user,
@@ -120,21 +121,21 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 7,
       title: 'Pay bills',
-      creationDate: new Date(2024, 4, 14), // April 14, 2024
+      creationDate: new Date(2024, 4, 14),
       description: 'Pay electricity, internet, and phone bills.',
       priority: 'High',
-      reminder: new Date(2024, 4, 16), // April 16, 2024 (reminder for two days ago)
-      status: 'ToDo', // Could be changed to 'InProgress' if you've started paying them.
+      reminder: new Date(2024, 4, 16),
+      status: 'ToDo',
 
       user,
     },
     {
       id: 8,
       title: 'Learn a new skill',
-      creationDate: new Date(2024, 4, 8), // April 8, 2024
+      creationDate: new Date(2024, 4, 8),
       description: 'Spend some time learning a new coding language or skill.',
       priority: 'Medium',
-      endDate: new Date(2024, 4, 15), // April 15, 2024 (past due)
+      endDate: new Date(2024, 4, 15),
       status: 'InProgress',
 
       user,
@@ -142,7 +143,7 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 9,
       title: 'Plan a weekend trip',
-      creationDate: new Date(2024, 4, 5), // April 5, 2024
+      creationDate: new Date(2024, 4, 5),
       description: 'Research and book a weekend getaway trip.',
       priority: 'Low',
       status: 'Done',
@@ -152,10 +153,10 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 10,
       title: 'Organize your workspace',
-      creationDate: new Date(2024, 4, 1), // April 1, 2024
+      creationDate: new Date(2024, 4, 1),
       description: 'Declutter and organize your desk or work area.',
       priority: 'Low',
-      endDate: new Date(2024, 4, 3), // April 3, 2024 (past due)
+      endDate: new Date(2024, 4, 3),
       status: 'Done',
 
       user,
@@ -163,11 +164,11 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 11,
       title: 'Meditate',
-      creationDate: new Date(2024, 4, 17), // April 17, 2024 (today)
+      creationDate: new Date(2024, 4, 17),
       description:
         'Spend some time meditating for mindfulness and stress relief.',
       priority: 'Low',
-      startDate: new Date(2024, 4, 17), // April 17, 2024 (today)
+      startDate: new Date(2024, 4, 17),
       status: 'ToDo',
 
       user,
@@ -175,10 +176,10 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 12,
       title: 'Write a thank-you note',
-      creationDate: new Date(2024, 4, 16), // April 16, 2024
+      creationDate: new Date(2024, 4, 16),
       description: 'Express gratitude to someone who helped you.',
       priority: 'Low',
-      endDate: new Date(2024, 4, 18), // April 18, 2024
+      endDate: new Date(2024, 4, 18),
       status: 'ToDo',
 
       user,
@@ -186,10 +187,10 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 13,
       title: 'Review bank statements',
-      creationDate: new Date(2024, 4, 15), // April 15, 2024
+      creationDate: new Date(2024, 4, 15),
       description: 'Check for any suspicious activity or errors.',
       priority: 'Medium',
-      reminder: new Date(2024, 4, 19), // April 19, 2024 (reminder for two days later)
+      reminder: new Date(2024, 4, 19),
       status: 'ToDo',
 
       user,
@@ -197,7 +198,7 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 14,
       title: 'Water the plants',
-      creationDate: new Date(2024, 4, 17), // April 17, 2024 (today)
+      creationDate: new Date(2024, 4, 17),
       description: 'Give your indoor and outdoor plants some water.',
       priority: 'Low',
       status: 'ToDo',
@@ -207,10 +208,10 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 15,
       title: 'Learn a new recipe',
-      creationDate: new Date(2024, 4, 12), // April 12, 2024
+      creationDate: new Date(2024, 4, 12),
       description: 'Find a new recipe and try cooking it.',
       priority: 'Medium',
-      endDate: new Date(2024, 4, 14), // April 14, 2024 (past due)
+      endDate: new Date(2024, 4, 14),
       status: 'InProgress',
 
       user,
@@ -218,10 +219,10 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 16,
       title: 'Backup important files',
-      creationDate: new Date(2024, 4, 10), // April 10, 2024
+      creationDate: new Date(2024, 4, 10),
       description: 'Back up your important documents and data.',
       priority: 'High',
-      reminder: new Date(2024, 4, 18), // April 18, 2024 (reminder for tomorrow)
+      reminder: new Date(2024, 4, 18),
       status: 'ToDo',
 
       user,
@@ -229,10 +230,10 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 17,
       title: 'Organize a social event',
-      creationDate: new Date(2024, 4, 8), // April 8, 2024
+      creationDate: new Date(2024, 4, 8),
       description: 'Plan a get-together with friends or family.',
       priority: 'Low',
-      endDate: new Date(2024, 4, 20), // April 20, 2024 (upcoming deadline)
+      endDate: new Date(2024, 4, 20),
       status: 'InProgress',
 
       user,
@@ -240,7 +241,7 @@ export function getTestTasksData(user): Partial<TaskEntity>[] {
     {
       id: 18,
       title: 'Clean out your inbox',
-      creationDate: new Date(2024, 4, 5), // April 5, 2024
+      creationDate: new Date(2024, 4, 5),
       description: 'Unsubscribe from unwanted emails and organize your inbox.',
       priority: 'Medium',
       status: 'Done',
